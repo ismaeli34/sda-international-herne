f302e63f64c0806ad1cc9779d6c9872400c900f9
ismaeli34 <ronneyismael@gmail.com>
2025-10-12 23:50:17 +0200

    header component fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,37 +1,35 @@
-import {AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {CommonModule, Location} from '@angular/common';
 import {Router, RouterModule} from '@angular/router';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
-import {AuthService} from '../services/auth.service';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import { ChangeDetectorRef } from '@angular/core';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 import {LanguageService} from '../services/language.service';
-import {CommonModule} from '@angular/common';
-import {animate, style, transition, trigger} from '@angular/animations';
-
+import {AuthService} from '../services/auth.service';
 @Component({
   selector: 'app-header',
-  imports: [TranslateModule, RouterModule, CommonModule],
+  standalone: true,
+  imports: [RouterModule, TranslateModule, CommonModule, MatAutocompleteModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
-  animations: [
-    trigger('fadeUp', [
-      transition(':enter', [ // when element enters DOM
-        style({ opacity: 0, transform: 'translateY(50px)' }),
-        animate('800ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-      ])
-    ])
-  ]
+  styleUrl: './header.component.scss'
 })
+
 export class HeaderComponent implements OnInit{
 
-  isMobile: boolean = false;
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50; // change color after 50px scroll
+  }
 
   languages: Language[] = [
     { lang: 'en', name: 'English', flag: '🇬🇧',  value: 'en', },
     { lang: 'de', name: 'German', flag: '🇩🇪', value: 'de' },
 
   ];
-  menuOpen = false;
-
   lang ='en';
 
   showAboutMenu = false;
@@ -42,21 +40,22 @@ export class HeaderComponent implements OnInit{
 
   isLoggedIn = false;
 
+
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
-  constructor(private router: Router,
+  constructor(private location: Location,
+              private router: Router,
               private translateService: TranslateService,
               private cdr: ChangeDetectorRef,
               private languageService: LanguageService,
-              private authService:AuthService,
+              private authService:AuthService
 
   ) {
 
 
   }
-
 
   isActive(route: string): boolean {
     return this.router.isActive(route, false);
@@ -64,11 +63,8 @@ export class HeaderComponent implements OnInit{
   }
 
 
-
-
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
-    console.log("Menu toggled:", this.isMenuOpen);
   }
 
   changeLang(selectedLanguage: string) {
@@ -78,9 +74,6 @@ export class HeaderComponent implements OnInit{
 
   ngOnInit() {
     // Subscribe to language changes from the service
-
-    this.checkScreen();
-    window.addEventListener('resize', () => this.checkScreen());
     this.lang = this.languageService.getCurrentLang();
 
 
@@ -96,10 +89,6 @@ export class HeaderComponent implements OnInit{
       this.isLoggedIn = status;
       this.cdr.detectChanges();
     });
-  }
-
-  checkScreen() {
-    this.isMobile = window.innerWidth <= 1024;
   }
 
   logout(){
